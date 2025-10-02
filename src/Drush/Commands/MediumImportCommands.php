69be21b28a671806d04d9d0e4353eeeff4db90a4
Eric Rasmussen <ericrasmussen1@gmail.com>
2025-10-02 14:47:18 -0500

    Updates to downgrade headings to paragraphs, get figcaption text, upgrade image size

--- src/Drush/Commands/MediumImportCommands.php
+++ src/Drush/Commands/MediumImportCommands.php
@@ -3,10 +3,11 @@
 namespace Drupal\medium_importer\Drush\Commands;
 
 use Drush\Commands\DrushCommands;
+use Drupal\Core\File\FileSystemInterface;
 use Drupal\file\Entity\File;
 use Drupal\media\Entity\Media;
+use Drupal\menu_link_content\Entity\MenuLinkContent;
 use Drupal\node\Entity\Node;
-use Drupal\Core\File\FileSystemInterface;
 use GuzzleHttp\ClientInterface;
 
 class MediumImportCommands extends DrushCommands {
@@ -35,6 +36,7 @@ class MediumImportCommands extends DrushCommands {
    *   Import all .html files found in that directory.
    */
   public function import($dir, $options = ['uid' => 1]) {
+
     $uid = isset($options['uid']) ? (int) $options['uid'] : 1;
 
     if (!is_dir($dir)) {
@@ -67,6 +69,9 @@ class MediumImportCommands extends DrushCommands {
         continue;
       }
 
+      // Get created date from the file name.
+      $created_date = explode('_', basename($file_path))[0];
+
       libxml_use_internal_errors(true);
       $doc = new \DOMDocument();
       $doc->loadHTML('<?xml encoding="utf-8" ?>' . $html);
@@ -74,8 +79,33 @@ class MediumImportCommands extends DrushCommands {
 
       $xpath = new \DOMXPath($doc);
 
+      // Get subtitle text (if present) for the summary.
+      $subtitle_nodes = $xpath->query('//section[@data-field="subtitle" and contains(@class, "p-summary")]');
+      $summary_text = '';
+      if ($subtitle_nodes->length) {
+        $summary_text = trim($subtitle_nodes->item(0)->textContent);
+      }
+
+      // Get the title.
       $title_nodes = $xpath->query('//h1[contains(@class, "p-name")]');
       $title = $title_nodes->length ? trim($title_nodes->item(0)->textContent) : basename($file_path);
+
+      // Remove the first <hr> in the body, if any, before the body is built.
+      $hr_nodes = $xpath->query('//section[@data-field="body"]//hr');
+      if ($hr_nodes->length > 0) {
+        $first_hr = $hr_nodes->item(0);
+        if ($first_hr->parentNode) {
+          $first_hr->parentNode->removeChild($first_hr);
+        }
+      }
+
+      // Remove any <h3> elements with graf--title class before the body is built.
+      $h3_nodes = $xpath->query('//section[@data-field="body"]//h3[contains(@class, "graf--title")]');
+      foreach ($h3_nodes as $h3) {
+        if ($h3->parentNode) {
+          $h3->parentNode->removeChild($h3);
+        }
+      }
 
       $body_nodes = $xpath->query('//section[@data-field="body"]');
       if ($body_nodes->length === 0) {
@@ -83,6 +113,28 @@ class MediumImportCommands extends DrushCommands {
         continue;
       }
       $body_node = $body_nodes->item(0);
+
+      // Downgrade headings to paragraphs.
+      foreach (['h2','h3','h4','h5','h6'] as $tag) {
+        $nodes = $xpath->query('.//' . $tag, $body_node);
+        foreach ($nodes as $node) {
+          $p = $doc->createElement('p');
+          foreach (iterator_to_array($node->childNodes) as $child) {
+            $p->appendChild($child->cloneNode(true));
+          }
+          if ($node->parentNode) {
+            $node->parentNode->replaceChild($p, $node);
+          }
+        }
+      }
+
+      // Find any <blockquote> in the body that contains the phrase "To stay up to date".
+      $blockquote_nodes = $xpath->query('.//blockquote[contains(., "To stay up to date")]', $body_node);
+      foreach ($blockquote_nodes as $bq) {
+        if ($bq->parentNode) {
+          $bq->parentNode->removeChild($bq);
+        }
+      }
 
       $img_nodes = [];
       $img_xpath_result = $xpath->query('.//img', $body_node);
@@ -102,6 +154,9 @@ class MediumImportCommands extends DrushCommands {
           $this->logger()->warning("Image without src encountered in {$file_path}, skipping image #{$index}");
           continue;
         }
+
+        // Change to a larger size image.
+        $src = preg_replace('#/max/(\d+)/#', '/max/2000/', $src);
 
         $this->logger()->notice("Downloading image: {$src}");
 
@@ -152,7 +207,7 @@ class MediumImportCommands extends DrushCommands {
             'name' => $basename,
             'field_media_image' => [
               'target_id' => $file->id(),
-              'alt' => $img->getAttribute('alt') ?: $title,
+              'alt' => $img->getAttribute('alt') ?: ' ',
             ],
             'status' => 1,
           ]);
@@ -183,7 +238,29 @@ class MediumImportCommands extends DrushCommands {
         }
 
         $media_uuid = $media->uuid();
-        $drupal_media_html = "<drupal-media data-entity-type=\"media\" data-entity-uuid=\"{$media_uuid}\"></drupal-media>";
+        $caption_text = '';
+
+        // Check if the image is inside a <figure> that has a <figcaption>.
+        $figure_node = $img->parentNode;
+        if ($figure_node && $figure_node->nodeName === 'figure') {
+          $caption_nodes = $figure_node->getElementsByTagName('figcaption');
+          if ($caption_nodes->length > 0) {
+            $caption_text = trim($caption_nodes->item(0)->textContent);
+            // Remove the <figcaption> so it doesn't appear in the body.
+            foreach ($caption_nodes as $caption_node) {
+              if ($caption_node->parentNode) {
+                $caption_node->parentNode->removeChild($caption_node);
+              }
+            }
+          }
+        }
+
+        if ($caption_text) {
+          $drupal_media_html = "<drupal-media data-entity-type=\"media\" data-align=\"center\" data-view-mode=\"wide\" data-entity-uuid=\"{$media_uuid}\" data-caption=\"" . htmlspecialchars($caption_text, ENT_QUOTES | ENT_SUBSTITUTE, 'UTF-8') . "\"></drupal-media>";
+        }
+        else {
+          $drupal_media_html = "<drupal-media data-entity-type=\"media\" data-align=\"center\" data-view-mode=\"wide\" data-entity-uuid=\"{$media_uuid}\"></drupal-media>";
+        }
 
         $fragment = $doc->createDocumentFragment();
         $fragment->appendXML($drupal_media_html);
@@ -200,16 +277,19 @@ class MediumImportCommands extends DrushCommands {
           'type' => 'featured_content',
           'title' => $title,
           'uid' => $uid,
+          'created' => strtotime($created_date),
+          'changed' => strtotime($created_date),
           'status' => 1,
           'body' => [
             'value' => $body_html,
             'format' => 'standard',
+            'summary' => $summary_text,
           ],
         ];
 
         if (!empty($media_entities)) {
           $lead_media = $media_entities[0];
-          $node_values['field_featured_content_lead_media'] = [
+          $node_values['featured_content_lead_media'] = [
             'target_id' => $lead_media->id(),
             'target_type' => 'media',
           ];
